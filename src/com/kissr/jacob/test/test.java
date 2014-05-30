01d3c7a87b09b7fe19b7dd23f66c8e16eb527b5f
taz40 <ldspcjunkie@gmail.com>
2014-05-30 16:24:18 -0600

    5/30/14 4:23 pm

---
+++ src/com/kissr/jacob/test/test.java
@@ -0,0 +1,42 @@
+package com.kissr.jacob.test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class test {
+	String[] confusedanswers ={"huh?","what?","wha?"};
+	Random rand = new Random();
+	public static void main (String[] args){
+		new test();
+	}
+	
+	public test(){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("hi");
+		delay("whats up?",600);
+		
+		while(true){
+			if (scan.nextLine().equals("nothing")){
+				delay("ok",800);
+				
+				delay("good bye",1500);
+				break;
+			}else{
+				delay(confusedanswers[rand.nextInt(confusedanswers.length )],800);
+			}
+		}
+	}
+	
+	public static void delay(String text, int time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(text);
+	}
+	
+}
